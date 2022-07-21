f9ebdef3b43ab407feea57524302e3f5d6116ec6
szekelypeter <szekely.pter.29@gmail.com>
2022-07-21 02:57:50 +0200

    tasks basics

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { TaskService } from "../services/task.service";
 
@@ -11,18 +12,26 @@ export class TasksComponent {
     phase = ""
     round = 0
 
-    questions = []
-    tasks = []
-    placesDesc = []
-    placesMaps=[]
-    placesImages=[]
+    answer = new FormControl("", [Validators.required])
+    controls = {
+        answer: this.answer
+    }
+
+    form = new FormGroup(this.controls)
+
+    roundTitle = ["első"]
+    question = ["question one?"]
+    questionTitle = ["question one?"]
+    label = ["yeah qeuestionone"]
+    task = ["This is a simple task"]
+    taskTitle = ["This is a simple task"]
+    placeTitle = ["Hotel annabella strandja"]
+    placesDesc = ["Go to here!"]
+    placesMaps=["https://www.google.com/maps/place/Hotel+Annabella+strandja/@46.9573625,17.8929885,16z/data=!4m8!1m2!2m1!1sanna+hotel+balaton!3m4!1s0x4769bddf72a8c2ff:0xa003588c2709805b!8m2!3d46.9556342!4d17.9000456"]
 
     constructor(private taskService: TaskService, private router: Router) {
         this.phase = taskService.phase
         this.round = taskService.round
-        sessionStorage.setItem("route", "../tasks")
-        sessionStorage.setItem("round", this.round.toString())
-        sessionStorage.setItem("phase", this.phase)
     }
 
     next(event: any) {
@@ -36,13 +45,17 @@ export class TasksComponent {
                 break;
             }
             case "question": {
-                this.phase = "place"
-                this.round++
-                break;
+                if (this.answer.valid) {
+                    this.phase = "place"
+                    this.round++
+                    break;
+                } else {
+                    this.form.markAllAsTouched()
+                    break;
+                }
+
             }
         }
-        sessionStorage.setItem("round", this.round.toString())
-        sessionStorage.setItem("phase", this.phase)
         this.router.navigate(
             [],
             { queryParams: { phase: this.phase, round: this.round } }
